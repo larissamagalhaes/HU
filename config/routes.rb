85bc915b0f30229ed7928e09c150d6a91449aa6e
Larissa Magalhaes <larissamayra.magalhaes@gmail.com>
2018-08-09 15:38:32 -0400

    create navBar and modal question

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'home#index'
+
+  get '/about' => 'home#about'
+
+  post '/questions' => 'home#temp'
+
+  get '/questions/:id' => 'home#question'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
